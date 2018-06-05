f5ffd21b167d0144052e3643e8b6652fcf2c063f
Tristan Matthias <hello@tristanmatthias.com>
2018-06-05 11:25:57 +1000

    Moved to yargs and added watch and glob

--- bin/sass-render.js
+++ bin/sass-render.js
@@ -1,115 +1,77 @@
 #!/usr/bin/env node
-
-const commandLineArgs = require('command-line-args');
-const commandLineUsage = require('command-line-usage');
-const recursive = require('recursive-readdir');
+const yargs = require('yargs');
 const path = require('path');
+const gaze = require('gaze');
+const glob = require('glob');
 const sassRender = require('../index.js').sassRender;
 
 (async() => {
-    const options = [
-        {
-            name: 'source',
-            alias: 's',
-            type: String,
-            description: 'Template file to render sass into.',
-            defaultOption: true,
-        },
-        {
-            name: 'directory',
-            alias: 'd',
-            type: String,
-            description: 'Directory to search for sass files to render sass into.',
-        },
-        {
-            name: 'output',
+    const o = yargs
+        .command('$0 <input>', 'Compile Sass to a template')
+        .option('output', {
             alias: 'o',
-            type: String,
-            description: 'Output file path',
-        },
-        {
-            name: 'template',
+            type: 'string',
+            describe: 'Output file path',
+        })
+        .option('template', {
             alias: 't',
-            type: String,
-            description: 'Template file to use, must use `<% content %>` as delimiter',
-            defaultValue: path.resolve(__dirname, '../template.js')
-        },
-        {
-            name: 'help',
+            type: 'string',
+            describe: 'Template file to use, must use `<% content %>` as delimiter',
+            default: path.resolve(__dirname, '../template.js')
+        })
+        .option('watch', {
+            alias: 'w',
+            type: 'boolean',
+            describe: 'Watch the file system for changes and render automatically'
+        })
+        .option('help', {
             alias: 'h',
-            type: Boolean,
-            description: 'Print this message.',
-        },
-        {
-            name: 'include',
+            type: 'boolean',
+            describe: 'Print this message.',
+        })
+        .option('include', {
             alias: 'i',
-            type: String,
-            description: 'Include directory for @imports (EG: node_modules)'
-        },
-        {
-            name: 'suffix',
+            type: 'string',
+            describe: 'Include directory for @imports (EG: node_modules)',
+            default: path.resolve(process.cwd(), 'node_modules')
+        })
+        .option('suffix', {
             alias: 'S',
-            type: String,
-            description: 'Suffix for the rendered file',
-            defaultValue: '-css.js'
-        },
+            type: 'string',
+            describe: 'Suffix for the rendered file',
+            default: '-css.js'
+        })
+        .option('quiet', {
+            alias: 'q',
+            type: 'boolean',
+            describe: 'No logs'
+        })
+        .argv;
 
-    ];
+    function render(fp) {
+        if (!o.quiet) console.log(`Rendering ${fp}...`);
 
-    const {
-        source,
-        directory,
-        output,
-        template,
-        help,
-        include,
-        suffix
-    } = commandLineArgs(options);
-
-
-    function printUsage() {
-        const sections = [{
-                header: 'sass-render',
-                content: 'Render sass into an element\'s lit template',
-            },
-            {
-                header: 'Options',
-                optionList: options,
-            },
-        ];
-        console.log(commandLineUsage(sections));
-    }
-
-    function error(err) {
-        console.error(err);
-        printUsage();
-        process.exit(-1);
-    }
-
-    if (help) {
-        printUsage();
-        process.exit(0);
-    }
-
-    if (source && directory) error('Please provide either the source or the directory, not both!');
-
-    if (source) {
-        sassRender(source, template, output, include, suffix).catch((err) => {
+        sassRender(fp, o.template, o.output, o.include, o.suffix).catch((err) => {
             console.error(err);
             process.exit(-1);
-        });
-    } else {
-        const files = await recursive(directory, [(f, stat) => {
-            if (!stat.isFile()) return false;
-            return f.split('.').pop() != 'scss';
-        }], (err, files) => {
-            files.forEach(f => {
-                sassRender(f, template, output, include, suffix).catch((err) => {
-                    console.error(err);
-                    process.exit(-1);
-                });
-            })
+        }).then(() => {
+            if (!o.quiet) console.log(`Complete!`);
         })
-
     }
-})()
+
+    glob(o.input, (err, files) => {
+        files.forEach(render);
+    });
+
+    if (o.watch) {
+        if (!o.quiet) console.log(`Watching ${o.input} for changes...`);
+
+        gaze(o.input, (err, watcher) => {
+            if (err) console.error(err);
+            // On file changed
+            watcher.on('changed', render);
+            // On file added
+            watcher.on('added', render);
+        });
+    }
+})();
